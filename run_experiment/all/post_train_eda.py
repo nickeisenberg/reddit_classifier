09d81027a1efaec0edc94a158c0b2520593b4603
nickeisenberg <nickeisenberg@gmail.com>
2024-04-20 18:07:51 -0700

    loss and accuracy plot

--- run_experiment/all/post_train_eda.py
+++ run_experiment/all/post_train_eda.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 train_loss_log = "loss_logs/train_log.csv"
 val_loss_log = "loss_logs/validation_log.csv"
 
@@ -8,8 +9,23 @@ train = pd.read_csv(train_loss_log)
 val = pd.read_csv(val_loss_log)
 
 fig, ax = plt.subplots(1, 2)
-ax[0].plot(train["total_loss"])
-ax[0].plot(val["total_loss"])
-ax[1].plot(train["accuracy"])
-ax[1].plot(val["accuracy"])
+ax[0].plot(
+    train["total_loss"].to_numpy().reshape((-1, 546)).mean(axis=1),
+    label="train loss"
+)
+ax[0].plot(
+    val["total_loss"].to_numpy().reshape((-1, 149)).mean(axis=1),
+    label="validation loss"
+)
+ax[1].plot(
+    train["accuracy"].to_numpy().reshape((-1, 546)).mean(axis=1),
+    label="train accuracy (%)"
+)
+ax[1].plot(
+    val["accuracy"].to_numpy().reshape((-1, 149)).mean(axis=1),
+    label="validation accuracy (%)"
+)
+ax[0].legend()
+ax[1].legend()
+fig.supxlabel("epochs")
 plt.show()
