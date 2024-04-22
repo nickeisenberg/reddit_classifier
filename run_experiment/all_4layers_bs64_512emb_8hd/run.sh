79c76eb792919379019d008d2a9ca7923741164f
nickeisenberg <nickeisenberg@gmail.com>
2024-04-22 08:06:14 -0700

    added 4 new experiments to try on HPC

---
+++ run_experiment/all_4layers_bs64_512emb_8hd/run.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+#SBATCH --job-name=model
+#SBATCH --output=out.%x.o%j.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:2
+#SBATCH --partition=pbatch
+#SBATCH --time=12:00:00
+
+srun /g/g11/eisenbnt/venvs/base/bin/python3 \
+    -u /g/g11/eisenbnt/GitRepos/reddit_classifier/run_experiment/all_4layers_bs64_512emb_8hd/__main__.py
+
