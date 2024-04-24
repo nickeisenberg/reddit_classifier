90f0e97e4204952ad2fe7336f0c1f20edcd64fdb
nickeisenberg <nickeisenberg@gmail.com>
2024-04-24 08:49:18 -0700

    pulled back results from hpc

--- run_experiment/all_6layers_bs64_256emb_4hd/post_train_eda.py
+++ run_experiment/all_6layers_bs64_256emb_4hd/post_train_eda.py
@@ -10,19 +10,19 @@ val = pd.read_csv(val_loss_log)
 
 fig, ax = plt.subplots(1, 2)
 ax[0].plot(
-    train["total_loss"].to_numpy().reshape((-1, 546)).mean(axis=1),
+    train["total_loss"].to_numpy().reshape((-1, 1091)).mean(axis=1),
     label="train loss"
 )
 ax[0].plot(
-    val["total_loss"].to_numpy().reshape((-1, 149)).mean(axis=1),
+    val["total_loss"].to_numpy().reshape((-1, 297)).mean(axis=1),
     label="validation loss"
 )
 ax[1].plot(
-    train["accuracy"].to_numpy().reshape((-1, 546)).mean(axis=1),
+    train["accuracy"].to_numpy().reshape((-1, 1091)).mean(axis=1),
     label="train accuracy (%)"
 )
 ax[1].plot(
-    val["accuracy"].to_numpy().reshape((-1, 149)).mean(axis=1),
+    val["accuracy"].to_numpy().reshape((-1, 297)).mean(axis=1),
     label="validation accuracy (%)"
 )
 ax[0].legend()
--- run_experiment/all_6layers_bs64_512emb_8hd/run.sh
+++ run_experiment/all_6layers_bs64_512emb_8hd/run.sh
@@ -1,12 +1,12 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-#SBATCH --job-name=6ly_bs64_512emb_8h
+#SBATCH --job-name=6ly
 #SBATCH --output=out.%x.o%j.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --partition=pbatch
-#SBATCH --time=03:00:00
+#SBATCH --time=12:00:00
 
 srun /g/g11/eisenbnt/venvs/base/bin/python3 \
     -u /g/g11/eisenbnt/GitRepos/reddit_classifier/run_experiment/all_6layers_bs64_512emb_8hd/__main__.py
