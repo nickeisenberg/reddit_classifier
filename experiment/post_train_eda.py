347754420ad4424849b3d5a255b5f2ccfd8f2b7a
nickeisenberg <nickeisenberg@gmail.com>
2024-04-26 09:44:11 -0700

    clean up

--- experiment/post_train_eda.py
+++ experiment/post_train_eda.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from torch import load
 
-loss_log_root = "./run_experiment/all_6layers_bs64_256emb_4hd/loss_logs"
+loss_log_root = "./experiment/loss_logs"
 train_loss_log = os.path.join(loss_log_root, "train_log.csv")
 val_loss_log = os.path.join(loss_log_root, "validation_log.csv")
 train = pd.read_csv(train_loss_log)
@@ -40,5 +40,5 @@ fig.supxlabel("epochs")
 fig.savefig(os.path.join(loss_log_root, "loss_and_accuracy.png"))
 plt.show()
 
-sd = "run_experiment/all_6layers_bs64_256emb_4hd/state_dicts/validation_ckp.pth"
+sd = "experiment/state_dicts/validation_ckp.pth"
 sd = load(sd, map_location="cpu")
